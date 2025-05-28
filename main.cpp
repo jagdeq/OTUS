73989179ff49f368c505bd46d926cfab40ad6df5
jagdeq <ans8nik@gmail.com>
2025-05-28 13:02:32 +0300

    Change main application file. Remove hardcode.

--- main.cpp
+++ main.cpp
@@ -5,36 +5,19 @@
 
 #include "async.h"
 
-constexpr size_t g_cmdBlockSize = 3;
-
-void doFirstThread()
+int main(int argc, char** argv)
 {
-    std::vector<std::string> buf = {"cmd1_1", "cmd2_1", "{",      "cmd3_1", "cmd4_1", "}", "{", "cmd5_1",  "cmd6_1",
-                                    "{",      "cmd7_1", "cmd8_1", "}",      "cmd9_1", "}", "{", "cmd10_1", "cmd11_1"};
+    if (argc != 2) {
+        std::cout << "Incorrect input. Set command line block.\n";
+        std::cout << "Example: ./async_cli 3\n";
+        return 0;
+    }
 
-    size_t context = async::connect(3);
-    for (std::string& cmd : buf)
+    auto context = async::connect(atoi(argv[1]));
+    for (std::string cmd; std::getline(std::cin, cmd);)
         async::receive(cmd.data(), cmd.size(), context);
 
     async::disconnect(context);
-}
 
-void doSecondThread()
-{
-    std::vector<std::string> buf{"cmd1_2", "cmd2_2", "cmd3_2", "cmd4_2", "cmd5_2"};
-    size_t context = async::connect(3);
-    for (std::string& cmd : buf)
-        async::receive(cmd.data(), cmd.size(), context);
-
-    async::disconnect(context);
-}
-
-int main()
-{
-    std::thread th(doSecondThread);
-    doFirstThread();
-    th.join();
-
-    while (true) {}
     return 0;
 }
